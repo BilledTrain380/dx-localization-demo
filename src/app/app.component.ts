a15af79cfecf8a3c8734f66330446f1c7cf900c2
Nicolas Märchy <nicolas.maerchy@xovis.com>
2020-01-07 11:26:34 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {locale} from 'devextreme/localization';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dx-localization-demo';
+  readonly title = 'dx-localization-demo';
+  readonly applyButton = {
+    text: 'Apply',
+    onClick: () => this.applyLocale(),
+  };
+
+  date = Date.now();
+  readonly format = {day: '2-digit', year: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+
+  currentLocale = 'en';
+  isLocaleApplying = false;
+
+  constructor() {
+    locale(this.currentLocale);
+  }
+
+  applyLocale(): void {
+    this.isLocaleApplying = true;
+
+    setTimeout(() => {
+      locale(this.currentLocale);
+      this.isLocaleApplying = false;
+    }, 250);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DxDateBoxModule, DxTextBoxModule} from 'devextreme-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxDateBoxModule,
+    DxTextBoxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
